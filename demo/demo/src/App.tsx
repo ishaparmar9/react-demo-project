c75a666fa6e4bf64c436c8c2351a5d26c5fac5f5
ishaparmar9 <101660450+ishaparmar9@users.noreply.github.com>
2023-06-19 02:52:34 +0530

    Add files via upload

---
+++ demo/demo/src/App.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Country from "./Pages/Country";
+import City from "./Pages/City";
+import Sidebar from "./Components/Sidebar";
+import AddCountry from "./Pages/AddCountry";
+import AddCity from "./Pages/AddCity";
+
+
+const App: React.FC = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleWidth = (isOpen:boolean) => {
+    setOpen(isOpen);
+  };
+
+  return (
+    <>
+      <Sidebar isOpen={handleWidth}/>
+      <div style={{marginLeft: !open ? '265px' : '90px', marginRight: '25px', transition: 'margin-left 0.25s ease'}}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/country" element={<Country />} />
+          <Route path="/city" element={<City />} />
+          <Route path="/country/view" element={<City />} />
+          <Route path="/country/add" element={<AddCountry />} />
+          <Route path="/country/edit" element={<City />} />
+          <Route path="/city/view" element={<City />} />
+          <Route path="/city/add" element={<AddCity />} />
+          <Route path="/city/edit" element={<City />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ demo/demo/src/Pages/AddCountry.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+
+const AddCountry = () => {
+  return (
+    <>
+      <Typography
+        variant="h5"
+        sx={{ fontWeight: "bold", marginBottom: "30px" }}
+      >
+        Add Country
+      </Typography>
+      <Grid
+        container
+        rowSpacing={1}
+        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Grid item xs={6}>
+          <TextField label="Country Name" id="countryName" size="small" sx={{width: '100%'}}/>
+        </Grid>
+        <Grid item xs={6}>
+          <TextField label="Country Name" id="countryName" size="small" sx={{width: '100%'}}/>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+export default AddCountry;
